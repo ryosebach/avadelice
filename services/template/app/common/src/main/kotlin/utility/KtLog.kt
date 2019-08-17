cc4f9f24453d325fa0180177b975a16a62d66f68
ryosebach <ryo.freezia@gmail.com>
2019-08-18 04:32:41 +0900

    :sparkles: Add Logger Utility.

---
+++ services/template/app/common/src/main/kotlin/utility/KtLog.kt
@@ -0,0 +1,26 @@
+package utility
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import kotlin.reflect.KProperty
+
+/**
+ * ロガー
+ * 下記のような形で利用する
+ * companion object {
+ *   val log by KtLog()
+ * }
+ * log.Info.......etc..
+ */
+open class KtLog {
+    var log: Logger? = null
+    operator fun getValue(thisRef: Any, property: KProperty<*>): Logger {
+        return log ?: LoggerFactory.getLogger(thisRef.javaClass.name.let {
+            val matchIndex = it.length - 10
+            when (it.lastIndexOf("\$Companion")) {
+                matchIndex -> it.substring(0, matchIndex)
+                else -> it
+            }
+        }).apply { this@KtLog.log = this }
+    }
+}
