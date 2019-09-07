0ea3ac2533c63dbdd1fff3519dbc813e875f84e1
ryosebach <ryo.freezia@gmail.com>
2019-09-08 04:48:38 +0900

    :sparkles: Add ExampleTable

---
+++ services/template/app/data-access/src/main/kotlin/rdb/table/ExampleTable.kt
@@ -0,0 +1,14 @@
+package rdb.table
+
+import org.jetbrains.exposed.dao.LongIdTable
+
+object ExampleTable : LongIdTable("example") {
+    val exampleKey = varchar("example_key", 191)
+    val nameJa = varchar("name_ja", 191)
+    val nameEn = varchar("name_en", 191)
+    val nameKo = varchar("name_ko", 191)
+    val nameZh = varchar("name_zh", 191)
+    val enabled = bool("enabled")
+    val createdAt = datetime("created_at")
+    val updatedAt = datetime("updated_at")
+}
