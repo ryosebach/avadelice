2eb5803716fb76d98872315f72889c7ec628f1d6
ryosebach <ryo.freezia@gmail.com>
2019-09-30 04:10:26 +0900

    :sparkles: Add test runner.

---
+++ services/template/app/infrastructure/src/test/kotlin/helper/TestRunner.kt
@@ -0,0 +1,72 @@
+package helper
+
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.PropertyNamingStrategy
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.ninja_squad.dbsetup.DbSetup
+import com.ninja_squad.dbsetup.Operations
+import com.ninja_squad.dbsetup.destination.DriverManagerDestination
+import com.ninja_squad.dbsetup.operation.Operation
+import com.typesafe.config.ConfigFactory
+import io.ktor.config.HoconApplicationConfig
+import io.ktor.server.testing.TestApplicationEngine
+import io.ktor.server.testing.createTestEnvironment
+import io.ktor.util.KtorExperimentalAPI
+import module.InfrastructureModule
+import org.koin.ktor.ext.installKoin
+
+@KtorExperimentalAPI
+class TestRunner(
+    module: org.koin.dsl.module.Module
+) {
+    private val testApplicationEngine: TestApplicationEngine
+
+    init {
+        val applicationConfig = HoconApplicationConfig(testConfig)
+        val modules = listOf(
+            module,
+            InfrastructureModule.module(applicationConfig)
+        )
+
+        testApplicationEngine = TestApplicationEngine(
+            createTestEnvironment {
+                config = applicationConfig
+            }
+        )
+
+        testApplicationEngine.start(true)
+
+        testApplicationEngine.application.installKoin(modules)
+    }
+
+    /**
+     * テストを用意したアプリケーションで実行する
+     */
+    fun withApplicationEngine(block: TestApplicationEngine.() -> Unit) {
+        with(testApplicationEngine, block)
+    }
+
+    companion object {
+        private val testConfig = ConfigFactory.load("application-test.conf")!!
+        val objectMapper = jacksonObjectMapper().apply {
+            configure(SerializationFeature.INDENT_OUTPUT, true)
+            configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            propertyNamingStrategy = PropertyNamingStrategy.SNAKE_CASE
+        }
+
+        /**
+         * DBの準備をする
+         */
+        fun setupDatabase(vararg operations: Operation) {
+            DbSetup(
+                DriverManagerDestination(
+                    testConfig.getString("database.master.url"),
+                    testConfig.getString("database.master.user"),
+                    testConfig.getString("database.master.password")
+                ),
+                Operations.sequenceOf(operations.asList())
+            ).launch()
+        }
+    }
+}
